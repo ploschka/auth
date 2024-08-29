79b0d114a28a3404cee26bb3e8d185bb4bb77f9b
Ploshka <folga12381@gmail.com>
2024-08-29 17:58:19 +1000

    Refresh path implemented

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -3,10 +3,12 @@ package server
 import (
 	"encoding/base64"
 	"encoding/json"
+	"io"
 	"net/http"
 	"strings"
 
 	"github.com/ploschka/auth/internal/auth"
+	"github.com/ploschka/auth/internal/mailer"
 	"github.com/ploschka/auth/internal/model"
 	"gorm.io/gorm"
 )
@@ -83,5 +85,127 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func refreshHandler(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
 
+	req := tokenResponse{}
+
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		badRequest(w)
+		return
+	}
+
+	rawRefresh, err := base64.RawURLEncoding.DecodeString(req.Refresh)
+	if err != nil {
+		badRequest(w)
+		return
+	}
+
+	rawRefresh, err = auth.DecryptToken(rawRefresh)
+	if err != nil {
+		badRequest(w)
+		return
+	}
+
+	refresh := &auth.RefreshToken{}
+	err = json.Unmarshal(rawRefresh, refresh)
+
+	valid := auth.CheckPair(req.Access, refresh)
+	if !valid {
+		unauthorized(w)
+		return
+	}
+
+	ip := r.RemoteAddr
+	ip, _, _ = strings.Cut(ip, ":")
+
+	user := model.User{
+		Guid: refresh.GUID,
+	}
+
+	db := model.GetDB()
+
+	q := func(d *gorm.DB) *gorm.DB {
+		return d.Where(&user).First(&user)
+	}
+
+	result := q(db)
+	if result.Error != nil {
+		unauthorized(w)
+		return
+	}
+
+	if !user.RefreshKey.Valid {
+		unauthorized(w)
+		return
+	}
+
+	rawHash, err := base64.RawURLEncoding.DecodeString(user.RefreshKey.String)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
+
+	valid = auth.Validate(rawRefresh, rawHash)
+	if !valid {
+		unauthorized(w)
+		return
+	}
+
+	if ip != refresh.Ip {
+		err = mailer.SendIpWarning(user.Email, ip)
+		if err != nil {
+			internalServerError(w)
+			return
+		}
+	}
+
+	var access string
+	access, refresh, err = auth.GenerateTokens(ip, user)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
+
+	rawRefresh, err = json.Marshal(refresh)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
+
+	encrypted, err := auth.EncryptToken(rawRefresh)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
+
+	hashed, err := auth.HashToken(rawRefresh)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
+
+	resp := tokenResponse{
+		Access:  access,
+		Refresh: base64.RawURLEncoding.EncodeToString(encrypted),
+	}
+
+	user.RefreshKey.String = base64.RawURLEncoding.EncodeToString(hashed)
+	user.RefreshKey.Valid = true
+
+	respJson, err := json.Marshal(resp)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
+
+	_, err = w.Write(respJson)
+	if err != nil {
+		internalServerError(w)
+		return
+	}
 }
